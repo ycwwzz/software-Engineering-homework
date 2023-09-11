5396997474b12eec817d3b29c3e037609bad6f88
mac <1796969317@qq.com>
2023-09-11 14:54:36 +0800

    add both algo

--- 3121004702/main.py
+++ 3121004702/main.py
@@ -1,11 +1,6 @@
 # 导入所需的库
 import difflib
-import sys
 
-import jieba # 中文分词库
-import nltk # 英文分词库
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer # TF-IDF加权方法
 
 # 定义一个函数来读取文件内容并返回一个字符串
 def read_file(file_name):
@@ -13,8 +8,8 @@ def read_file(file_name):
         content = f.read()
     return content
 
-if __name__ =="__main__":
 
+if __name__ == "__main__":
     # original_file = sys.argv[1]  # 原文文件路径
     # plagiarized_file = sys.argv[2]  # 抄袭版文件路径
     # output_file = sys.argv[3]  # 输出文件路径
@@ -25,14 +20,12 @@ if __name__ =="__main__":
     # with open(plagiarized_file, 'r', encoding='utf-8') as f:
     #     plagiarized_text = f.read()
 
-
     # 定义原文文件和抄袭版文件的路径
     original_file = 'original.txt'
     plagiarized_file = 'plagiarized.txt'
     # 读取文件内容
     original_text = read_file(original_file)
     plagiarized_text = read_file(plagiarized_file)
-
 
     # 计算余弦相似度
     similarity = difflib.SequenceMatcher(None, original_text, plagiarized_text).ratio()
@@ -41,15 +34,10 @@ if __name__ =="__main__":
     repetition_rate = similarity
     # 输出结果
 
-
-
-
-
-
     # 将相似度转换为百分比形式
-    # repetition_percentage = round(repetition_rate * 100, 2)
+    repetition_percentage = round(repetition_rate * 100, 2)
 
-    print(f'重复率为{repetition_rate}')
+    print(f'重复率为{repetition_percentage}%')
 
     # 将结果写入输出文件
     # with open(output_file, 'w', encoding='utf-8') as f:
